0f4bc8102d5148eabbbd050c403b4b3f8c30c973
stefi23 <stefania.rosca2309@gmail.com>
2020-04-25 17:51:24 +0200

    fixes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,9 +12,9 @@ const MyNavbar = () => (
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/about">About</Nav.Link>
-              <Nav.Link href="/tags">Tags</Nav.Link>
+              <Nav.Link href="/blog">Home</Nav.Link>
+              <Nav.Link href="/blog/about">About</Nav.Link>
+              <Nav.Link href="/blog/tags">Tags</Nav.Link>
             </Nav>
             {/* <Nav>
               <Nav.Link href="https://stefi.codes/">Portfolio</Nav.Link>
