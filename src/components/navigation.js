ea03497bd2254740ace4a78615fd5e6bc476417e
stefi23 <stefania.rosca2309@gmail.com>
2022-01-02 20:45:36 +0100

    updated sidebar nav buttons

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,18 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const NavBtn = ({ label, to = "#", isDisabled = false }) => {
+const NavBtn = ({ label, to = "#", isDisabled = false, direction }) => {
   if (isDisabled) {
     //To change what is returned
     return (
-      // <button disabled className="">
-      //   {label}
-      // </button>
       <p></p>
     )
   }
+  if(!isDisabled)
   return (
-    <Link to={to} className="p-1">
+    <Link to={to} className={`p-1 navigationButton navigationButton-${direction}`}>
       {label}
     </Link>
   )
@@ -20,25 +18,26 @@ const NavBtn = ({ label, to = "#", isDisabled = false }) => {
 
 export default ({ prefix = "/page/", currentPage, totalPages }) => {
   return (
-    //To change what it looks like
     <div
       className="row mt-2 nav-innerContent"
       style={{ borderRadius: "4px" }}
     >
       <div className="col-md-12 navigation-card">
         <div className="row">
-          <div className="col-6 p-2 d-flex justify-content-start pl-4 hover-bright">
+          <div className="col-6 p-2 d-flex align-items-center justify-content-start pl-3">
             <NavBtn
               to={currentPage == 2 ? `/` : `${prefix}/${currentPage - 1}`}
-              label={currentPage - 1}
+              label={`← Previous page: ${currentPage - 1}`}
               isDisabled={currentPage == 1 || currentPage == undefined}
+              direction="left"
             />
           </div>
-          <div className="col-6 p-2 d-flex justify-content-end pr-4 hover-bright">
+          <div className="col-6 p-2 d-flex align-items-center justify-content-end pr-3">
             <NavBtn
               isDisabled={currentPage == totalPages}
               to={`${prefix}/${currentPage + 1}`}
-              label={currentPage + 1}
+              label={`Next page: ${currentPage + 1} →`}
+              direction="right"
             />
           </div>
         </div>
