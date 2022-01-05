f9e51b2a29cce5c8c7c92ef1c1486b6ddd8a5b39
stefi23 <stefania.rosca2309@gmail.com>
2022-01-05 20:05:41 +0100

    Updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,23 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+
+ <div className="post-innerContent">
+        <div className="article-card row">
+          <div className="col">
+            <h1 className='post-innerContent-title'>
+             Not Found 😞
+            </h1>
+            <p className="post-innerContent-content">
+              You just hit a route that doesn&#39;t exist... the sadness.
+            </p>
+
+
+          </div>
+        </div>
+      </div>
+
   </Layout>
 )
 
