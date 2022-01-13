3d917de4e28b8dc43f61086f51b539c54a7db1dd
stefi23 <stefania.rosca2309@gmail.com>
2022-01-13 23:12:20 +0100

    remove helmet import'

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Helmet from "react-helmet"
 import "../components/layout.css"
 
 export default ({ data }) => {
