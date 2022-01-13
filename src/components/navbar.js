0d074befd70ce2b715438b5b8d029b8a0f6e106e
stefi23 <stefania.rosca2309@gmail.com>
2022-01-13 21:57:34 +0100

    Removed boostrap navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Nav, Navbar } from "react-bootstrap"
 
 const MyNavbar = () => (
   <header>
     <div className="row">
       <div className="col-md-12 mt-3">
-        <Navbar collapseOnSelect expand="lg" bg="light" className="rounded">
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
+          <div className="navBar rounded">
               <Link to="/" className='navLink'>Home</Link>
               <Link to="/tags" className='navLink'>Tags</Link>
               <Link to="/about" className='navLink'>About</Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+            </div>
       </div>
     </div>
   </header>
