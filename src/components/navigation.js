11e72ecc83201a0b302f45bb4cf01ab7854bcab1
Stefi Rosca <stefania.rosca2309@gmail.com>
2022-02-11 16:36:16 +0100

    fix errors on navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,35 +4,34 @@ import { Link } from "gatsby"
 const NavBtn = ({ label, to = "#", isDisabled = false, direction }) => {
   if (isDisabled) {
     return <p></p>
-    }
+  }
 
   return (
-    <Link to={to} className={`p-1 navigationButton navigationButton-${direction}`}>
+    <Link
+      to={to}
+      className={`p-1 navigationButton navigationButton-${direction}`}
+    >
       {label}
     </Link>
   )
 }
 
-export default ({ prefix = "/page", currentPage, totalPages }) => {
-
+const Navigation = ({ prefix = "/page", currentPage, totalPages }) => {
   return (
-    <div
-      className="row mt-2 nav-innerContent"
-      style={{ borderRadius: "4px" }}
-    >
+    <div className="row mt-2 nav-innerContent" style={{ borderRadius: "4px" }}>
       <div className="col-md-12 navigation-card">
         <div className="row">
           <div className="col-6 p-2 d-flex align-items-center justify-content-start pl-3">
             <NavBtn
-              to={currentPage == 2 ? `/` : `${prefix}/${currentPage - 1}`}
+              to={currentPage === 2 ? `/` : `${prefix}/${currentPage - 1}`}
               label={`← Previous page: ${currentPage - 1}`}
-              isDisabled={currentPage == 1 || currentPage == undefined}
+              isDisabled={currentPage === 1 || currentPage === undefined}
               direction="left"
             />
           </div>
           <div className="col-6 p-2 d-flex align-items-center justify-content-end pr-3">
             <NavBtn
-              isDisabled={currentPage == totalPages}
+              isDisabled={currentPage === totalPages}
               to={`${prefix}/${currentPage + 1}`}
               label={`Next page: ${currentPage + 1} →`}
               direction="right"
@@ -43,3 +42,5 @@ export default ({ prefix = "/page", currentPage, totalPages }) => {
     </div>
   )
 }
+
+export default Navigation
